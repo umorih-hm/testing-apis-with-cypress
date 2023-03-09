e75b7930ad43396e931cbf8f908cdb37b48b3fc9
h_kuramoto <h.kuramoto@bravetechnology.co.jp>
2023-03-09 17:34:33 +0900

    init

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://todo-app-barkend.herokuapp.com/',
+    integrationFolder: 'cypress/e2e/api-tests',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
