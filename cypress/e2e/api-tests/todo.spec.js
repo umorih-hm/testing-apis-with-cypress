89dc202fd75d65eb9cd4853c2c872b1753fa2576
h_kuramoto <h.kuramoto@bravetechnology.co.jp>
2023-03-09 17:50:57 +0900

    init

--- cypress/e2e/api-tests/todo.spec.js
+++ cypress/e2e/api-tests/todo.spec.js
@@ -1,10 +1,6 @@
 describe('TODO api testing', () => {
   let todoItem;
   it('fetches Todo items - GET', () => {
-      cy.request('/todos/').as('todoRequest');
-      cy.get('@todoRequest').then(todos => {
-          expect(todos.status).to.eq(200);
-          assert.isArray(todos.body, 'Todos Response is an array')
-      });
+    cy.visit('https://bravetechnology.co.jp/');
   });
 });
