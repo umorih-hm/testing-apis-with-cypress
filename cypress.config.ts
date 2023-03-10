fad7cbb8e536e435698517bc48b3da27f78100a4
h_kuramoto <h.kuramoto@bravetechnology.co.jp>
2023-03-10 17:36:50 +0900

    init

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://todo-app-barkend.herokuapp.com/',
-    specPattern: 'cypress/e2e/api-tests',
+    specPattern: 'cypress/e2e',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/api-tests/visit.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('TODO api testing', () => {
-  let todoItem;
-  it('fetches Todo items - GET', () => {
-    cy.visit('https://bravetechnology.co.jp/');
-  });
-});
